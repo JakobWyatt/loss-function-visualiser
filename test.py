c705819bc8b874225fee30ec52e122f3358aa5b8
Jakob Wyatt <jakob.wyatt@gmail.com>
2018-08-30 13:44:51 +0800

    added explanation of matplotlib3d provided on discord to source code

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import sys
 print(sys.path)
 
+"""Thankyou so much for the help, after playing around with the function
+    more this morning and looking at a tutorial on numpy broadcasting I
+    think i mostly understand it.
+Turns out that each element in the X, Y, Z array corresponds with eachother
+    to form one point (for example (X[0][1], Y[0][1], Z[0][1]) is a point
+    plotted on the grid). The order and shape of the 2d array dictate how
+    the mesh is constructed on the graph. Still don't exactly understand
+    how the mesh construction happens, but the most 'sane' results occur
+    via the output of numpy.meshgrid, where every element of either a mesh
+    or column is equal, and increasing over the matrix. (Eg. [1,2,3],[1,2,3])
+Also turns out that the reason the function accepts so many inputs is
+    because of the call to numpy.broadcast at the start, which is
+    similar to numpy.meshgrid for two inputs.
+Just writing this out here to help set it in my mind. Thanks again for the help"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
